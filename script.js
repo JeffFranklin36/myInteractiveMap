ca33005863428cb7a32dd726e0b96de50c40b760
Jeffrey <jeff.1001@live.com>
2022-04-19 14:58:46 -0500

    user popup added

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Create a map
-
-
 //obtain users location
 async function getCoords(){
  pos = await new Promise((resolve, reject) => {
@@ -14,24 +11,21 @@ async function getCoords(){
 (async () => {
  let userLocation = await getCoords()
  console.log(userLocation)
- let myMap = L.map('map').setView(userLocation, 13);
+ let myMap = L.map('map').setView(userLocation, 15);
  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
 }).addTo(myMap);
+let userMarker = L.marker(userLocation).addTo(myMap)
+let userPopup= L.popup()
+ .setLatLng(userLocation)
+ .setContent('<p>You are Here</p>')
+ .openOn(myMap)
+
+
 })();
 
 
 console.log(getCoords());
-
-
-
-// Get the user's coordinates: 
-// let userLocation = getCoords().then(userLocation => userLocation.pos)
-// console.log(userLocation)
-// let lat = getCoords().then(lat => userLocation[0])
-// let long = getCoords().then(long => userLocation.pos.longitude)
-
-//creste a map centered on users location
 
 //finding 5 nearest businesses of a specific type
